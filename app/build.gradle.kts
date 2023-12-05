bf90d6140b1840a40c86d15bbc37dd2160583b6c
Bekzat-Chauvbayev <kazna51k@gmail.com>
2023-12-06 02:06:03 +0600

    marsapiservice added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,7 +69,8 @@ android {
 
 dependencies {
 
-    // Import the Compose BOM
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+
     implementation(platform("androidx.compose:compose-bom:2023.06.01"))
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation("androidx.compose.material3:material3")
@@ -78,6 +79,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
+    implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
 
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
